1eb1a5469b2a47d004661df4768f678e0d1faa73
Rasmus Nordahl <rasmus.nordahl@student.tabyenskilda.se>
2022-03-28 14:24:33 +0200

    Co-authored-by: AngelieEmling <AngelieEmling@users.noreply.github.com> Co-authored-by: OlleGranquist <OlleGranquist@users.noreply.github.com>

--- adventuretime.py
+++ adventuretime.py
@@ -1,5 +1,39 @@
 
+class Inventory:
+	armor = "Clothing"
+	lockpick = False 
+
+
 
 print('big')
+
+yes = ["y", "yes"]
+no = ["n", "no"]
+
+ans = input(">>")
+
+def main():
+    main_menu()
+
+    rooms = {
+        'Captains quarters': {'name': 'Captains quarters', 'Down': 'Kitchen and dining', 'item': 'nothing'},
+        'Kitchen and dining': {'name': 'Kitchen and dining', 'Up': 'Captains quarters', 'Down': 'Sleeping quarters',
+                            'item': 'nothing'},
+        'Sleeping quarters': {'name': 'Sleeping quarters', 'Up': 'Kitchen and dining', 'Down': 'Engine Room', 
+                            'Outside': 'Deck', 'item': 'nothing'},
+        'Engine Room': {'name': 'Engine Room', 'Up': 'Sleeping quarters', 'item': 'nothing'},
+        'Deck': {'name': 'Deck', 'item': 'nothing'}
+    }
+
+    # current location 
+    cur_location = rooms['Captains quarters']
+
+    # current commands available to the player
+    commands = ['North', 'South', 'West', 'East', 'Up', 'Down', 'Item']
+
+    # Blank list for the inventory
+    inventory = []
+
+
 
 
