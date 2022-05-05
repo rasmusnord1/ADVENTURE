8195d8c0a528804de0588c9a3d4e2122c5a432c3
Rasmus Nordahl <rasmus.nordahl@student.tabyenskilda.se>
2022-05-05 19:54:14 +0200

    DET FUNKAR ATT GÅ TILL ANDRA RUM NU

--- Testing_file.py
+++ Testing_file.py
@@ -1,11 +1,71 @@
-file = open('Text_support.txt')
+# file = open('Text_support.txt')
   
-# read the content of the file opened
-content = file.readlines()
+# # read the content of the file opened
+# content = file.readlines()
   
-# read 10th line from the file
-print("tenth line")
-print(content[1])
+# # read 10th line from the file
+# print("tenth line")
+# print(content[1])
+
+
+
+# gamestate = {}
+# gamestate["inventory"] = []
+# gamestate["current location"] = "kitchen"
+
+# commands = {}
+
+# def can_pickup_crowbar(gamestate):
+#     return gamestate["current location"] == "captains quarters"
+
+# def pickup_crowbar(gamestate):
+#     gamestate["inventory"].append("crowbar")
+
+# commands["pickup crowbar"] = {
+#     "can_perform": can_pickup_crowbar,
+#     "perform":pickup_crowbar
+# }
+
+
+# print(commands)
+
+# print(gamestate)
+# print(commands["pickup crowbar"]["can_perform"](gamestate)) # True om det går
+# commands["pickup crowbar"]["perform"](gamestate)
+# print(gamestate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# rooms = {
+#     'captains quarters': {'name': 'captains quarters', 'roomchoice': 'deck', 'item': 'crowbar', 'desc' : 'line[10]'},
+#     'kitchen': {'name': 'kitchen', 'roomchoice': 'sleeping quarters', 'item': '[TEXT]', 'desc' : 'line[2]'},
+#     'sleeping quarters': {'name': 'sleeping quarters', 'roomchoice': ['kitchen','engine room', 'deck'], 'item': '[TEXT]', 'desc' : 'line[3]'},
+#     'engine room': {'name': 'engine room', 'roomchoice': 'sleeping quarters', 'item': '[TEXT]', 'desc' : 'line[4]'},
+#     'deck': {'name': 'deck', 'roomchoice': ['captains quarters', 'sleeping quarters'], 'item': '[TEXT]', 'desc' : 'line[5]'}
+# }
+
+# cur_location = rooms['captains quarters']
+
+
+# print('{}'.format(cur_location['item']))
 
 
 #Gamla rooms-systemet, utan roomchoice. 
--- adventuretime.py
+++ adventuretime.py
@@ -1,6 +1,6 @@
 from gamestate import *
-#import time
-#- för att använda time.sleep(x)
+import time
+
 file = open('Text_support.txt')
 line = file.readlines()
 
@@ -12,16 +12,132 @@ line = file.readlines()
 
 
 
+gamestate = {}
+gamestate['inventory'] = []
+gamestate['current location'] = 'captains quarters'
+commands = {}
+
+
+rooms = {
+    'captains quarters': {'name': 'captains quarters', 'roomchoice': 'deck', 'item': 'crowbar', 'desc' : line[8]},
+    'kitchen': {'name': 'kitchen', 'roomchoice': 'sleeping quarters', 'item': '[TEXT]', 'desc' : 'line[2]'},
+    'sleeping quarters': {'name': 'sleeping quarters', 'roomchoice': ['kitchen','engine room', 'deck'], 'item': '[TEXT]', 'desc' : line[3]},
+    'engine room': {'name': 'engine room', 'roomchoice': 'sleeping quarters', 'item': '[TEXT]', 'desc' : 'line[4]'},
+    'deck': {'name': 'deck', 'roomchoice': ['captains quarters', 'sleeping quarters'], 'item': '[TEXT]', 'desc' : 'line[5]'}
+}
+command_switch = {
+    ('pickup','pick-up','take','get','grab'):'take',
+    ('use','apply','operate'):'use',
+    ('go','walk','run','leave','climb'):'go',
+    ('look','inspect','view','l'):'look'
+}
 
 
 
-def main(): #kanske ta bort funktionen
-#    main_menu()
-    print(f'\n\n{line[0]}, ')
+cur_location = rooms['sleeping quarters']
+
+def parse_input(text_input):
+    words = text_input.split(' ')
+    helper_commands = ['at','to','in','through','by','the','and','but','inside','toward']
+    for word in words[:]:
+        if word in helper_commands: 
+            words.remove(word)
+    for command in command_switch:
+        if words[0] in command:
+            text_output = f'{command_switch[command]} {" ".join(words[1:])}'
+            ri_output = " ".join(words[1:])
+            print(text_output)
+            return text_output, ri_output
 
 
-    # current location 
-    cur_location = 'captains quarters'
+            # if '{}'.format(cur_location['item']) in text_input:
+            #     ri_output = '{}'.format(cur_location['item'])
+            #     print('{}'.format(commands[command]))
+            #     return commands[command], ri_output
+            # for roomcheck in rooms[cur_location['name']] or rooms[cur_location['roomchoice']]:
+            #     if roomcheck in text_input:
+            #         ri_output = roomcheck
+            #         return commands[command], ri_output
+
+            # return 
+            # output = f'{commands[command]} {" ".join(words[1:])}'
+
+            # print(output)
+            # return output, None
+
+def usage_error():
+    print('You cannot do that here')
+
+def can_pickup_item(text_output):
+    print(f'{text_output}' == f"take {cur_location['item']}")
+    return f'{text_output}' == f"take {cur_location['item']}"
+
+def pickup_item(text_output):
+    if can_pickup_item(text_output):
+        print('bing')
+        cur_item = cur_location['item']
+        if cur_location['item'] == 'nothing':
+            print(f"You've already picked up {cur_item}.")
+            return
+        gamestate['inventory'].append(cur_item)
+        print(f"You picked up the {cur_item}.")
+        cur_location['item'] = 'nothing'
+    else:
+        return usage_error()
+
+
+def can_use_item():
+    return True
+
+def use_item():
+    print('[TRIGGER] use item')
+    if gamestate['current location'] == 'deck':
+        gamestate['inventory'].remove('crowbar')
+        print(line[...])
+
+def can_goto_location(text_output):
+    words = text_output.split()
+    print(words[0])
+    if words[0] == 'go':
+        print(' '.join(words[1:]))
+        print(cur_location['roomchoice'])
+        print(' '.join(words[1:]) in cur_location['roomchoice'])
+        return ' '.join(words[1:]) in cur_location['roomchoice']
+
+def goto_location(text_output):
+    if can_goto_location(text_output):
+        words = text_output.split()
+        print(words)
+        cur_location = rooms[' '.join(words[1:])]
+        print(cur_location) 
+
+
+print(cur_location['item'])
+print(cur_location['roomchoice'])
+# def goto_location(ri_output):
+#     print(ri_output)
+#     if ri_output in rooms[cur_location['roomchoice']]:
+#         cur_location = rooms[ri_output]
+#         print(cur_location)
+#         return cur_location
+#     else:
+#         return usage_error
+
+
+
+
+
+
+
+
+
+# def main(): #kanske ta bort funktionen
+# #    main_menu()
+#     print(f'\n\n{line[0]}')
+
+
+#     # current location 
+#     cur_location = 'captains quarters'
 N = True
 #def main_game():
 while N == True:
@@ -29,23 +145,23 @@ while N == True:
 #        print('This room contains {}.'.format(cur_location['item'], ))  # Print the item in the locations, BEHÖVS NOG INTE
         input(f"\n\n{''.join(line[1:4])} \nPress enter to continue.")
         
-        print('\n'*7 + f"You are in the {(cur_location['name'])}.\n{''.join(line[8])}")
-        text_output, room_output = parse_input(input('\n\nWhat would you like to do?\n>> '))
+        print('\n'*7 + f"You are in the {(cur_location['name'])}.\n{''.join(cur_location['desc'])}")
+        
+        text_output, ri_output = parse_input(input('\n\nWhat would you like to do?\n>> '))
         
         
         N = False
 
 commands['take item'] = {
-    'perform': pickup_item
+    'perform': pickup_item(text_output)
 }
 commands['use item'] = {
-    'perform': use_item
-}
-commands['go', room_output] = {
-    'perform': goto_location(room_output)
+    'perform': use_item()
 }
 
-
+commands['goto location'] = {
+    'perform': goto_location(text_output)
+}
 
 
 
