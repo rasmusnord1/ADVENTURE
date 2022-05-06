eeca92302046904a106b9161e69cb53be6ee178a
Rasmus Nordahl <rasmus.nordahl@student.tabyenskilda.se>
2022-05-06 23:26:43 +0200

    FIX: moving to other locations

--- adventuretime.py
+++ adventuretime.py
@@ -16,7 +16,7 @@ line = file.readlines()
 gamestate = {}
 commands = {}
 gamestate['inventory'] = []
-# gamestate['current location'] = 'captains quarters'
+
 
 
 
@@ -37,11 +37,9 @@ command_switch = {
 
 
 
-cur_location = rooms['captains quarters']
+gamestate['current location'] = rooms['captains quarters']
 
-def dialogue():
-    Repeat = True
-    return Repeat
+
 
 def parse_input(text_input):
     words = text_input.split(' ')
@@ -62,22 +60,22 @@ def parse_input(text_input):
 
 def usage_error():
     input('\nYou cannot do that here')
-    return #dialogue
+    return main()
 
 def can_pickup_item(text_output):
-    print(f'{text_output}' == f"take {cur_location['item']}")
-    return f'{text_output}' == f"take {cur_location['item']}"
+    print(f'{text_output}' == f"take {gamestate['current location']['item']}")
+    return f'{text_output}' == f"take {gamestate['current location']['item']}"
 
 def pickup_item(text_output):
     if can_pickup_item(text_output):
         print('bing')
-        cur_item = cur_location['item']
-        if cur_location['item'] == 'nothing':
+        cur_item = gamestate['current location']['item']
+        if gamestate['current location']['item'] == 'nothing':
             print(f"You've already picked up {cur_item}.")
-            return
+            return main()
         gamestate['inventory'].append(cur_item)
         input(f"\nYou picked up the {cur_item}.")
-        cur_location['item'] = 'nothing'
+        gamestate['current location']['item'] = 'nothing'
     else:
         return usage_error()
 
@@ -86,20 +84,21 @@ def use_item(text_output, gamestate):
     words = text_output.split()
     if words[1] in gamestate['inventory']:
         if 'use' and 'on' in words:
-            if words[1] == cur_location['useitem']:
-                if words[3] == cur_location['use_item_on']:
-                    input(f"You succesfully used your {words[1]} on the {words[3]}.")
-                    gamestate['inventory'].remove([words[1]])
-                    Success = cur_location['item_succes'] = True
+            if words[1] == gamestate['current location']['useitem']:
+                if words[3] == gamestate['current location']['use_item_on']:
+                    print(words, words[1], gamestate['inventory'])
+                    input(f"\nYou succesfully used your {words[1]} on the {words[3]}.")
+                    gamestate['inventory'].remove(words[1])
+                    Success = gamestate['current location']['item_succes'] = True
                     return Success
                 input("\nNothing happened.")
-                return #dialogue
+                return main()
             input("\nYou cannot use that item here.")
-            return #dialogue
+            return main()
         input("\nYou have to specify what to use the item on.")
-        return #dialogue
+        return main()
     input("\nYou don't have that item in your inventory.")
-    return #dialogue
+    return main()
 
 # def use_item(text_output, gamestate):
 #     if can_use_item(text_output, gamestate):
@@ -113,30 +112,30 @@ def can_goto_location(text_output):
     print(words[0])
     if words[0] == 'go':
         print(' '.join(words[1:]))
-        print(cur_location['roomchoice'])
-        print(' '.join(words[1:]) in cur_location['roomchoice'])
-        return ' '.join(words[1:]) in cur_location['roomchoice']
+        print(gamestate['current location']['roomchoice'])
+        print(' '.join(words[1:]) in gamestate['current location']['roomchoice'])
+        return ' '.join(words[1:]) in gamestate['current location']['roomchoice']
 
-def goto_location(text_output):
+def goto_location(text_output, gamestate):
     if can_goto_location(text_output):
         words = text_output.split()
         print(words)
-        cur_location = rooms[' '.join(words[1:])]
-        print(cur_location)
-        input(f"\nYou walk to the {cur_location['name']}") 
-        return cur_location
+        gamestate['current location'] = rooms[' '.join(words[1:])]
+        input(f"\nYou walk to the {gamestate['current location']['name']}") 
+        return main()
     return usage_error()
 
 #Room-specific tasks
 def radio(text_output):
     if 'on' and 'radio' in text_output:
         input(f"\n{''.join(line[20])}")
+        return main()
 
 
 
 # TESTING
-print(cur_location['item'])
-print(cur_location['roomchoice'])
+print(gamestate['current location']['item'])
+print(gamestate['current location']['roomchoice'])
 # def goto_location(ri_output):
 #     print(ri_output)
 #     if ri_output in rooms[cur_location['roomchoice']]:
@@ -147,7 +146,31 @@ print(cur_location['roomchoice'])
 #         return usage_error
 
 
+def main():
+        cur_location = gamestate['current location']
+        print('\n'*7 + f"You are in/on the {cur_location['name']}.\n{''.join(cur_location['desc'])}")
+        
+        print(cur_location['name'])
+        print(cur_location['item'])
+        print(cur_location['use_item_on'])
+        print(cur_location['roomchoice'])
 
+        text_output = parse_input(input('\n\nWhat would you like to do?\n>> ').lower())
+        print('_____')
+        print(text_output)
+        if text_output == None:
+            return usage_error()
+        elif 'go' in text_output:
+            return goto_location(text_output, gamestate)
+        elif 'take' in text_output:
+            return pickup_item(text_output)
+        elif 'use' in text_output:
+            return use_item(text_output, gamestate)
+        elif 'turn' in text_output:
+            return radio(text_output)
+        else:
+            usage_error()
+        return
 
 
 
@@ -159,37 +182,38 @@ print(cur_location['roomchoice'])
 #     print(f'\n\n{line[0]}')
 
 input(f"\n\n{''.join(line[1:4])} \nPress enter to continue.")
+main()
 
 #     # current location 
 #     cur_location = 'captains quarters'
-Repeat = True
+Repeat = False
 #def main_game():
-while Repeat == True:
-#        print('You are in the {}.'.format(cur_location['name'], ))  # Print current location
-#        print('This room contains {}.'.format(cur_location['item'], ))  # Print the item in the locations, BEHÖVS NOG INTE
+# while Repeat == True:
+# #        print('You are in the {}.'.format(cur_location['name'], ))  # Print current location
+# #        print('This room contains {}.'.format(cur_location['item'], ))  # Print the item in the locations, BEHÖVS NOG INTE
         
-        print('\n'*7 + f"You are in/on the {(cur_location['name'])}.\n{''.join(cur_location['desc'])}")
+#         print('\n'*7 + f"You are in/on the {(cur_location['name'])}.\n{''.join(cur_location['desc'])}")
         
-        text_output = parse_input(input('\n\nWhat would you like to do?\n>> ').lower())
-        print('_____')
-        print(text_output)
-        if text_output == None:
-            usage_error()
-        elif 'go' in text_output:
-            goto_location(text_output)
-        elif 'take' in text_output:
-            pickup_item(text_output)
-        elif 'use' in text_output:
-            use_item(text_output, gamestate)
-        elif 'turn' in text_output:
-            radio(text_output)
-        else:
-            usage_error()
+#         text_output = parse_input(input('\n\nWhat would you like to do?\n>> ').lower())
+#         print('_____')
+#         print(text_output)
+#         if text_output == None:
+#             usage_error()
+#         elif 'go' in text_output:
+#             goto_location(text_output)
+#         elif 'take' in text_output:
+#             pickup_item(text_output)
+#         elif 'use' in text_output:
+#             use_item(text_output, gamestate)
+#         elif 'turn' in text_output:
+#             radio(text_output)
+#         else:
+#             usage_error()
         
-        print(cur_location['name'])
-        print(cur_location['item'])
-        print(cur_location['use_item_on'])
-        print(cur_location['roomchoice'])
+#         print(cur_location['name'])
+#         print(cur_location['item'])
+#         print(cur_location['use_item_on'])
+#         print(cur_location['roomchoice'])
         
         # Gamla commands av Linus:
         #
